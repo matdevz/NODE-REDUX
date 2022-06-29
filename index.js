6be57c502fc1c3627260e0b7ea355816546f4c0f
Mateus Henrique <mateusdzn10@gmail.com>
2022-06-29 09:03:51 -0400

    Style: Separando os elementos do redux

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
 const redux = require('redux');
+const counterReducer = require('./reducers/CounterReducer');
+const listReducer = require('./reducers/ListReducer');
+const actionCounter = require('./actions/CounterAction');
+const actionList = require('./actions/ListAction');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
 
-const incrementAction = (value) => ({ type: 'INCREMENT', payload: value || 1 });
-const decrementAction = (value) => ({ type: 'DECREMENT', payload: value || 1 });
-
-function counterReducer(state = 0, action) {
-	switch (action.type) {
-		case 'INCREMENT':
-			return state + action.payload;
-		case 'DECREMENT':
-			return state - action.payload;
-		default:
-			return state;
-	}
-}
-
-const addItemAction = (item) => ({ type: 'ADD_ITEM', payload: item });
-
-function listReducer(state = [], action) {
-	switch (action.type) {
-		case 'ADD_ITEM':
-			return [...state, action.payload];
-		default:
-			return state;
-	}
-}
 const allReducers = combineReducers({
-	counter: counterReducer,
-	list: listReducer,
+	counter: counterReducer.counter,
+	list: listReducer.list,
 });
 const store = createStore(allReducers);
 store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(incrementAction(10));
-store.dispatch(decrementAction(7));
+store.dispatch(actionList.addItem('Leaning Git and Redux'));
+store.dispatch(actionList.addItem('Learning React and Node'));
+
+store.dispatch(actionCounter.increment(10));
+store.dispatch(actionCounter.decrement(7));
